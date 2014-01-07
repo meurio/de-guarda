7cdc52ebd9556694b18212482e139cf9c0aaec83
Josias Schneider <josias.ds@gmail.com>
2014-01-07 11:41:02 -0200

    Rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeGuarda::Application.config.secret_key_base = 'a37a5ed8b75051bd6aa522915107d766edc0ae9e01793ca86b2cac86efc32d1ae5a0bdf80b8e83f29e5e4569fcf69741d56e1ec43cbd2211546e4a5d9542f17b'
